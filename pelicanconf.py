918d26f1409c877ac5381921719bc8852b3cf2f6
Brian Dunnette <dunn0172@umn.edu>
2021-05-04 16:10:07 -0500

    adding themes (again again)

--- pelicanconf.py
+++ pelicanconf.py
@@ -47,4 +47,4 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-THEME='themes/pelican-alchemy/alchemy'
+THEME='themes/alchemy/alchemy'
