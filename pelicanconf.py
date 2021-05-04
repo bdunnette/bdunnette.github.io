e5858002854d40ba174c20ff39011ab7d76b4768
Brian Dunnette <dunn0172@umn.edu>
2021-05-04 15:51:48 -0500

    updating themes

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,15 +13,15 @@ TIMEZONE = 'US/Central'
 
 DEFAULT_LANG = 'en'
 
-CC_LICENSE = {
-    "name": "Creative Commons Attribution-ShareAlike 4.0 International License",
-    "version": "4.0",
-    "slug": "by-sa",
-    "icon": True,
-    "language": "en_US",
-}
+# CC_LICENSE = {
+#     "name": "Creative Commons Attribution-ShareAlike 4.0 International License",
+#     "version": "4.0",
+#     "slug": "by-sa",
+#     "icon": True,
+#     "language": "en_US",
+# }
 
-COPYRIGHT_YEAR = datetime.now().year
+# COPYRIGHT_YEAR = datetime.now().year
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -31,10 +31,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (
@@ -47,4 +47,4 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-THEME='simple-bootstrap'
+THEME='themes/pelican-alchemy/alchemy'
